766ce62f1c025d790c1ac3c37764b1d03da12f57
Maikelsuwisto <36490948+Maikelsuwisto@users.noreply.github.com>
2019-09-13 19:28:28 +1200

    Update EmployerProfile.jsx

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/EmployerProfile.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/EmployerProfile.jsx
@@ -52,7 +52,7 @@ export default class EmployeeProfile extends React.Component {
     loadData() {
         var cookies = Cookies.get('talentAuthToken');
         $.ajax({
-            url: 'http://localhost:60290/profile/profile/getEmployerProfile',
+            url: 'https://talentprofileserv.azurewebsites.net/profile/profile/getEmployerProfile',
             headers: {
                 'Authorization': 'Bearer ' + cookies,
                 'Content-Type': 'application/json'
